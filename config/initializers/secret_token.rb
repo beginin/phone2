a6501b4d2b0731ef48027250f4e509121932fefa
Vladimit <beginin@mail.ru>
2013-01-29 11:28:47 +0800

    Добавление контроллера

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phone2::Application.config.secret_token = '8ef0f10bdbe538a87c97c211007c5a1df0489d18c7a8a4765f476a3f7998884b63f63f56bff08f612768128307c988f9bb39cbc84c44d208fd46fb03ccae176e'
