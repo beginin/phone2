ba48f2011b26dce8197fa8df2f14f31006705ee8
Vladimit <beginin@mail.ru>
2013-01-30 14:04:20 +0800

    Добавление проверки на номерах

--- app/models/tnumber.rb
+++ app/models/tnumber.rb
@@ -1,3 +1,11 @@
 class Tnumber < ActiveRecord::Base
   attr_accessible :block, :datein, :dateout, :sortnumber, :tarif_id, :voicenumber
+  belongs_to :tarif
+  
+  validates :voicenumber, :presence => true, 
+                          :length => { :is => 10 }, 
+                          :numericality => { :only_integer => true }, 
+                          :uniqueness => true
+  validates :sortnumber, :uniqueness => true, :length => { :is => 7 }, :allow_blank => true
+  validates :tarif_id, :presence => true
 end
