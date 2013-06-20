7d7ecc6a79f9db2285eba31a483e4e9c92c4f70b
Vladimir <beginin@mail.ru>
2013-06-20 13:13:32 +0800

    Добавление шапки

--- app/models/calllog.rb
+++ app/models/calllog.rb
@@ -1,4 +1,4 @@
 class Calllog < ActiveRecord::Base
-  attr_accessible :, :coast, :date, :descriptioncall_id, :duration, :load_id, :tnumber,
+  attr_accessible :coast, :date, :descriptioncall_id, :duration, :load_id, :tnumber,
    :tnumber_in, :tnumber_out, :typecall_id, :typeconnect_id, :valuemb, :base
 end
